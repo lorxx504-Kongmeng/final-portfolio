66f8f43a4780411921428232f67aae7490216c7a
kongmenglor <lorxx504@umn.edu>
2023-07-18 17:14:13 -0500

    pushing

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,8 +4,14 @@ const About = () => {
   return (
     <div className='w-full h-max text-center break-normal'>
         <div className='p-4'>
-            <div className='mx-auto w-full h-full'>
-                <h1 className='text-2xl'>About</h1>
+            <div className='mx-auto w-[60rem] h-full '>
+                <div className='flex justify-start'>
+                    <div className='flex'>
+                        <h1 className='text-blue text-4xl font-bold'>1. </h1>
+                        <h1 className='text-4xl font-bold '>About</h1>
+                    </div>
+
+                </div>
                 <h2 className="text-6xl mt-2">Who I Am.</h2>
                 <p className='mt-2 font-light'>Hello, my name is Kongmeng Lor and I am a Full Stack Engineer hailing from Minnesota. I hold a Bachelor's degree in Information Technology Infrastructure of Data Management from the University of Minnesota, Twin Cities, supplemented by a minor in Computer Science. In addition, I have successfully completed the York Solution B2E Full Stack Software Engineer Bootcamp. As I advance in my software development career, I am continuously enhancing my skills and knowledge. If you have any job openings or opportunities available, I would greatly appreciate the chance to connect with you.</p>
             </div>
--- components/main/main.jsx
+++ components/main/main.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 const Main = () => {
   return (
-    <div className='w-full h-screen text-center break-normal shadow-xl'>
+    <div className='w-full h-screen text-center break-normal'>
         {/* <div style={{position: 'relative', height: '200px'}}>
                     <Image src="/main/shot.png" fill></Image>
                     <div className='text-white'>
@@ -17,15 +17,15 @@ const Main = () => {
         <div className='w-full h-full mx-auto p-2 flex justify-center items-center'>
 
             <div className='p-2 max-w-[600px]'>
-                <div className='max-w-[120px] md:max-w-[1px] md:text-5xl text-2xl flex pl-12'>                    
+                <div className='max-w-[120px] md:pl-12 md:text-5xl text-2xl flex pl-14 text-gray-600'>                    
                     <h1 className='ml-2'>Hello,</h1>
-                    <h1 className='ml-2'>I'm</h1>
+                    <h1 className='ml-2 '>I'm</h1>
                 </div>
                 <div className='flex justify-center text-5xl font-bold md:text-8xl md:max-w-[480px] max-w-[300px] pl-48 md: md:pl-80 '>
                     <h1 className=''>{data.first_Name}</h1>
                     <h1 className='ml-4'>{data.last_Name}.</h1>
                 </div>
-                <div className= 'pl-10 md:pl-8 mt-4 '>
+                <div className= 'pl-10 md:pl-8 mt-4 text-gray-600'>
                     <p className='text-xl font-lgiht'>{data.description}</p>
                 </div>
                 <div className='flex items-center justify-center max-w-[330px] m-auto py-4 '>
@@ -37,7 +37,6 @@ const Main = () => {
                                         <FaGithub />
                                     </Link>
                             </div>    
-
                     </div>
             </div>
 
