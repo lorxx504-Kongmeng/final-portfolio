150d55076b06cb7075be357aa829ca78c6998eeb
kongmenglor <lorxx504@umn.edu>
2023-07-19 02:36:53 -0500

    about is done.

--- app/experiences/page.jsx
+++ app/experiences/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Experiences = () => {
   return (
-    <div>Experiences</div>
+    <div className='w-full h-screen text-center break-normal flex justify-center'>
+      <div className='max-w-[1400px] flex justify-center w-full h-full'> 
+          <div className='py-24'>
+            
+          </div>
+      </div>
+    </div>
   )
 }
 
