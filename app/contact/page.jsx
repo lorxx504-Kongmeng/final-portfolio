75c9140507685429c53dc4230710cc98e457f351
kongmenglor <lorxx504@umn.edu>
2023-07-19 14:53:59 -0500

    projects section is done.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='h-screen w-full bg-slate-800 text-white'>
+      
+      <div className='flex justify-center items-center py-8'>
+        <div>
+          <h1 className='text-4xl font-extrabold'>Contact.</h1>
+          <div>
+            <p>Interested in hiring me? Clock here to view my resume!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
