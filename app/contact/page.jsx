b7910a7f3b5fde2e107b5ef846c056b1bc7f7d0b
kongmenglor <lorxx504@umn.edu>
2023-07-19 15:23:43 -0500

    almost done, just need linkedin and github.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Contact = () => {
   return (
-    <div className='h-screen w-full bg-slate-800 text-white'>
+    <div className='h-screen w-full bg-slate-800 text-white text-center break-normal'>
       
-      <div className='flex justify-center items-center py-8'>
+      <div className='flex justify-center py-24 w-full h-full mx-auto'>
         <div>
-          <h1 className='text-4xl font-extrabold'>Contact.</h1>
-          <div>
-            <p>Interested in hiring me? Clock here to view my resume!</p>
+          <h1 className='text-4xl font-extrabold'>Get In Touch.</h1>
+          <div className='flex justify-center w-[22rem] mt-6'>
+            <p>Whether you have inquiries about code or just want to say hello, feel free to reach out to me anytime. My inbox is open for you. I'll try my best to get back to you!</p>
           </div>
+          
+          <div className='mt-2'>
+            <div className='shadow-xl rounded bg-white px-2 py-1 text-black' >
+                <Link href="/">Resume</Link>
+            </div>
+
+            
+
+          </div>
+
+          
         </div>
       </div>
     </div>
