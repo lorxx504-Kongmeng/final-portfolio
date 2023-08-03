a3a3d727359f808f0917f18be7eee2c3500e8018
kongmenglor <lorxx504@umn.edu>
2023-08-02 23:54:59 -0500

    trying to fix footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-        <footer className='text-sm text-center font-thin p-3 bg-slate-800 text-white w-[460px] h-full sm:w-full'>
-            <p>Developed & Designed</p>
-            <p>By Kongmeng Lor</p>
-            <p>July 2023</p>
+        <footer className="w-full h-screen text-center break-normal">
+            <div className='text-sm text-center font-thin p-3 bg-slate-800 text-white h-full sm:w-full'>
+              <p>Developed & Designed</p>
+              <p>By Kongmeng Lor</p>
+              <p>July 2023</p>
+            </div>
         </footer>
   )
 }
